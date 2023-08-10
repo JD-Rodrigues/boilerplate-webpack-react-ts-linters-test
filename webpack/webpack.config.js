5c57bd55b06710beb5a7a0b109e682588a869824
JD_Rodrigues <jdevproject01@gmail.com>
2023-08-10 16:18:52 -0300

    chore: add source-map-loader rule in webpack.config

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -22,6 +22,11 @@ module.exports = {
       {
         test: /\.(sa|sc|c)ss$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader']
       }
     ]
   },
