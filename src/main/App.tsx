50aed6f61b6a29d472cd50bfb511f1b8e7c3d6b5
JD_Rodrigues <jdevproject01@gmail.com>
2023-08-15 21:53:33 -0300

    chore: create type declarations to image files in order to import it as ts modules

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,7 +1,17 @@
 import './styles.scss'
+import logo from './assets/images/raw-logo.png'
+import fonte from './assets/images/fonte.png'
+import Envelope from './assets/images/envelope.svg'
 
 const App: React.FC = () => {
-  return <p className="teste">Olá, pessoal!!</p>
+  return (
+    <>
+      <p className="teste">Olá, pessoal!!</p>
+      <img src={logo} />
+      <img src={fonte} />
+      <Envelope />
+    </>
+  )
 }
 
 export default App
