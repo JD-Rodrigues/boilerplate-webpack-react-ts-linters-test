a41a6d565c76b371f1938537bb47e20a49a2bff0
JD_Rodrigues <jdevproject01@gmail.com>
2023-08-25 08:01:04 -0300

    chore: changes .scss files into modules

--- src/presentation/components/Button/index.tsx
+++ src/presentation/components/Button/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import styles from './styles.module.scss'
 
 const ButtonExample = () => {
-  return <button>Clique aqui!</button>
+  return <button className={styles.button}>Clique aqui!!</button>
 }
 
 export default ButtonExample
